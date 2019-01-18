68b7c2262bd955d10d39a0a8436ceb259c46f6a7
mikel <mikel@iltech.com>
2019-01-17 18:06:54 -0700

    point link to old blog

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -8,7 +8,7 @@ export default class Blog extends Component {
       <div className="__blog-container">
         <h1>Under Construction</h1>
         <span>We are currently working on revamping my blog, you can still visit my old one by the link bellow</span>
-        <h2>Visit Old Site</h2>
+        <h2><a href="https://blog.haileynorth.net/">Visit Old Site</a></h2>
       </div>
     )
   }
